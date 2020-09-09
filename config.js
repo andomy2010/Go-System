0398ac279f8bdb1d47ae4fcfb7cb44a7a47eba2e
andomy2010 <71041027+andomy2010@users.noreply.github.com>
2020-09-09 22:13:33 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzQ4NTcxMTAyMjUwNDAxODIz.X0fXKg.X36EI9Hj7JDnu6QQBB5tBDhibgks", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "=",
+  devs: ["634802791541637173"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+};
